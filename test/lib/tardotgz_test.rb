062188faf9a024c11d1429c4680c0ab0e2fc5ed2
Jonathan Hoyt <hoyt@github.com>
2015-02-22 22:17:41 -0800

    Test and implement #read_from_archive

--- test/lib/tardotgz_test.rb
+++ test/lib/tardotgz_test.rb
@@ -37,4 +37,53 @@ class TardotgzTest < Minitest::Test
       create_archive(source_path, archive_path)
     end
   end
+
+  def test_read_from_archive_returns_contents_of_file_matching_pattern_string
+    archive_path = path_helper("test/fixtures/archive.tar.gz")
+    pattern      = "testing1.md"
+
+    assert_equal "hello world\n", read_from_archive(archive_path, pattern)
+  end
+
+  def test_read_from_archive_returns_contents_of_files_matching_pattern_regexp
+    archive_path     = path_helper("test/fixtures/archive.tar.gz")
+    pattern          = /testing\d\.md/
+
+    assert_equal "hello world\nfoobarbaz\n", read_from_archive(archive_path, pattern)
+  end
+
+  def test_read_from_archive_yields_block_for_each_file_matching_pattern
+    archive_path     = path_helper("test/fixtures/archive.tar.gz")
+    pattern          = /testing\d\.md/
+    expected_results = [
+      "hello world\n",
+      "foobarbaz\n"
+    ]
+
+    result = read_from_archive(archive_path, pattern) do |tarfile|
+      assert expected_results.include?(tarfile.read)
+    end
+
+    assert_nil result
+  end
+
+  def test_read_from_archive_raises_error_if_archive_does_not_exist
+    archive_path = path_helper("foobarbaz.tar.gz")
+
+    assert_raises(Errno::ENOENT) do
+      read_from_archive(archive_path, "foo")
+    end
+  end
+
+  def test_read_from_archive_raises_error_if_pattern_does_not_match_any_files
+    archive_path = path_helper("test/fixtures/archive.tar.gz")
+
+    assert_raises(Errno::ENOENT) do
+      read_from_archive(archive_path, "foobarbaz")
+    end
+
+    assert_raises(Errno::ENOENT) do
+      read_from_archive(archive_path, /drunken\/noodle/)
+    end
+  end
 end
